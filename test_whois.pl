b797a69b0acdd1ef493f7658db6e618d7df073c7
Daniil Popov <popov.daniil@gmail.com>
2011-10-26 17:40:03 +0400

    i think about it at home

--- test_whois.pl
+++ test_whois.pl
@@ -5,22 +5,14 @@ use strict;
 
 use Net::Whois::Raw;
 use DBI;
-
 my @array_of_domains = @ARGV;
-my $database = 'whois';
-my $hostname = '127.0.0.1';
-my $port     = 3306;
-my $user     = 'root';
-my $password = 'solid';
 
-my $dsn = "DBI:mysql:database=$database;host=$hostname;port=$port";
-my $dbh = DBI->connect( $dsn, $user, $password ) or die 'connect error';
-
+my $dbh = DBI->connect( 'DBI:mysql:whois', 'root', 'solid' ) or die "Connection Error: $DBI::errstr\n";
 
 for  my $domain_name (@ARGV) {
-  $dbh->do( "INSERT INTO result  (name, info) VALUES (?,?)",
-    undef, $domain_name, whois $domain_name ) or print $domain_name . "\n" . whois $domain_name;
-  
+  $dbh -> do ("INSERT INTO result  (name, info) VALUES (?, ?)", undef, $domain_name, whois ($domain_name))
+  or die "$DBI::errstr\n";
+
 }
 
 $dbh->disconnect;
