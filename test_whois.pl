9a75a8a4a1472962bf6ac055c641d075ee771e47
Daniil Popov <popov.daniil@gmail.com>
2011-10-27 23:43:47 +0400

    change database

--- test_whois.pl
+++ test_whois.pl
@@ -7,13 +7,15 @@ use Net::Whois::Raw;
 use DBI;
 my @array_of_domains = @ARGV;
 
-my $dbh = DBI->connect( 'DBI:mysql:whois', 'root', 'solid' ) or die "Connection Error: $DBI::errstr\n";
-$dbh -> do ("START TRANSACTION");
-for  my $domain_name (@ARGV) {
-  $dbh -> do ("INSERT INTO result  (name, info) VALUES (?, ?)", undef, $domain_name, whois ($domain_name))
-  or die "$DBI::errstr\n";
+my $dbh = DBI->connect( 'DBI:mysql:whois', 'root', 'solid' )
+  or die "Connection Error: $DBI::errstr\n";
+$dbh->do("START TRANSACTION");
+for my $domain_name (@ARGV) {
+    $dbh->do( "INSERT INTO result  (name, info) VALUES (?, ?)",
+        undef, $domain_name, whois($domain_name) )
+      or die "$DBI::errstr\n";
 
 }
-$dbh -> do ("COMMIT");
+$dbh->do("COMMIT");
 $dbh->disconnect;
 
