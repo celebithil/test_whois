825bad09289724e73cf63b3b375d9f9c68d2f1bd
Daniil Popov <popov.daniil@gmail.com>
2011-10-27 23:40:59 +0400

    add transaction

--- test_whois.pl
+++ test_whois.pl
@@ -8,12 +8,12 @@ use DBI;
 my @array_of_domains = @ARGV;
 
 my $dbh = DBI->connect( 'DBI:mysql:whois', 'root', 'solid' ) or die "Connection Error: $DBI::errstr\n";
-
+$dbh -> do ("START TRANSACTION");
 for  my $domain_name (@ARGV) {
   $dbh -> do ("INSERT INTO result  (name, info) VALUES (?, ?)", undef, $domain_name, whois ($domain_name))
   or die "$DBI::errstr\n";
 
 }
-
+$dbh -> do ("COMMIT");
 $dbh->disconnect;
 
