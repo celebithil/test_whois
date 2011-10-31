d430bc12313dc1af8c7dac5d51c45330a4f96e0e
Daniil Popov <popov.daniil@gmail.com>
2011-10-31 16:21:20 +0400

    final version

--- test_whois.pl
+++ test_whois.pl
@@ -5,12 +5,14 @@ use strict;
 
 use Net::Whois::Raw;
 use DBI;
+
+# В дальнейшем @ARGV можно заменить массивом значений полученных из базы данных
 my @array_of_domains = @ARGV;
 
 my $dbh = DBI->connect( 'DBI:mysql:whois', 'root', 'solid' )
   or die "Connection Error: $DBI::errstr\n";
 $dbh->do("START TRANSACTION");
-for my $domain_name (@ARGV) {
+for my $domain_name (@array_of_domains) {
     $dbh->do( "INSERT INTO result  (name, info) VALUES (?, ?)",
         undef, $domain_name, whois($domain_name) )
       or die "$DBI::errstr\n";
